2502b8f84311e5c1715beba64c8732aadbb7f192
Zachary <syyun@snu.ac.kr>
2019-08-09 08:18:42 +0900

    Add start code of cpi considered gold

--- CPI_GOLD.py
+++ CPI_GOLD.py
@@ -1,6 +1,12 @@
 import utils
 
-gold_path = "data/Gold_monthly"
+data_path = {"Gold" : "data/Gold_monthly.pkl",
+             "CPI" : "data/CPI.pkl"}
 
-df_gold = utils.load
-df_cpi = utils.load_pickle(data_path)
+df_gold = utils.load_pickle(data_path["Gold"])
+df_cpi = utils.load_pickle(data_path["CPI"])
+
+df_gold
+
+if __name__ == "__main__":
+    pass
